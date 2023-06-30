5bca6f477130eb95114d97628711d2eace7e49e2
christoseteoglou <itsmeglue@gmail.com>
2023-06-30 12:54:32 +0200

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Christos Eteoglou on 2023-06-30.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
